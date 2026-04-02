be2be758d6d86bc0343b0993ae6d5f65263f2cc0
Juan Andrade <andrade.juan@outlook.com.br>
2026-04-02 14:14:25 -0300

    feat: remove separator of default theme

--- src/lib/terminal/ps1.ts
+++ src/lib/terminal/ps1.ts
@@ -1,5 +1,5 @@
 export const PS1_PROMPT_BASH =
-  ' __pnex_prompt(){ local exit_code="$?"; local d="$(pwd)"; printf "\n\n__PNEX_EXIT__%s__PNEX_EXIT____PNEX_CWD__%s__PNEX_CWD__" "$exit_code" "$d"; }; PROMPT_COMMAND=__pnex_prompt; PS1="  "; clear\r';
+  ' __pnex_prompt(){ local exit_code="$?"; local d="$(pwd)"; printf "\n__PNEX_EXIT__%s__PNEX_EXIT____PNEX_CWD__%s__PNEX_CWD__" "$exit_code" "$d"; }; PROMPT_COMMAND=__pnex_prompt; PS1="  "; clear\r';
 
 export const PS1_PROMPT_PSW =
   'function prompt { $code = if ($?) { 0 } else { 1 }; $d=(Get-Location).Path; Write-Host -NoNewline "`n__PNEX_EXIT__${code}__PNEX_EXIT____PNEX_CWD__${d}__PNEX_CWD__"; "  "; }; cls\r\r';
