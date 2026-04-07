86e71fb9a2a2d854e0dba20562c97c82d7336475
Juan Andrade <andrade.juan@outlook.com.br>
2026-04-07 11:46:18 -0300

    feat: turn simple ps1 prompts

--- src/lib/terminal/ps1.ts
+++ src/lib/terminal/ps1.ts
@@ -1,5 +1,5 @@
 export const PS1_PROMPT_BASH =
-  ' __pnex_prompt(){ local exit_code="$?"; local d="$(pwd)"; printf "\\033]7777;exit=%s\\007\\033]7777;cwd=%s\\007" "$exit_code" "$d"; }; PROMPT_COMMAND=__pnex_prompt; PS1="\n  "; clear\r';
+  ' PS1="\\[\\e]7777;exit=\$?\\a\\e]7777;cwd=\${PWD}\\a\\]\n  "; clear\r';
 
 export const PS1_PROMPT_PSW =
   'function prompt { $code = if ($?) { 0 } else { 1 }; $d=(Get-Location).Path; Write-Host -NoNewline ("$([char]27)]7777;exit=${code}$([char]7)$([char]27)]7777;cwd=${d}$([char]7)"); "`n  "; }; cls\r\r';
